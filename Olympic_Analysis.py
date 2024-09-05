4459b7ed4528ebc068cc08a2ca1559882f2a717c
Trancadit <ozairk048@gmail.com>
2024-09-06 01:08:50 +0500

    Olympic_Analysis.py

--- Olympic_Analysis.py
+++ Olympic_Analysis.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
+import scipy
 import PreprocessingFile, helper
 
 df = pd.read_csv("athlete_events.csv")
